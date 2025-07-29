e0c1b43d46cfe4373329f91990534d61d735223d
longvibol <vibollong@gmail.com>
2025-07-29 16:53:59 +0700

    binding

--- Week1/01-starting-project/src/app/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
- selector: 'app-header',
- standalone: true,
- templateUrl : './header.Component.html',
-})
-
-export class HeaderComponent{}
